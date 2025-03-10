fe1c96444a8da4d9eeba524cc940991db25a3bbc
aylinuner <unerayliin@gmail.com>
2025-03-11 01:47:49 +0300

    _layout klasörü düzeltildi, product_category eklendi
    sql ile product_category oluşturulup scaffold çekildi

--- Models/EcommerceDbContext.cs
+++ Models/EcommerceDbContext.cs
@@ -21,6 +21,8 @@ public partial class EcommerceDbContext : DbContext
 
     public virtual DbSet<product> products { get; set; }
 
+    public virtual DbSet<product_category> product_categories { get; set; }
+
     public virtual DbSet<user> users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -72,6 +74,21 @@ public partial class EcommerceDbContext : DbContext
                 .HasColumnType("decimal(3, 2)");
             entity.Property(e => e.size).HasMaxLength(50);
             entity.Property(e => e.sku).HasMaxLength(50);
+            entity.Property(e => e.update_date).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<product_category>(entity =>
+        {
+            entity.HasKey(e => e.id).HasName("PK_productCategory");
+
+            entity.ToTable("product_category");
+
+            entity.Property(e => e.create_date).HasColumnType("datetime");
+            entity.Property(e => e.description).HasMaxLength(200);
+            entity.Property(e => e.name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.price).HasColumnType("decimal(10, 2)");
             entity.Property(e => e.update_date).HasColumnType("datetime");
         });
 
